bd232f21406d4033e21944cbcbd0f1be4e9d8d93
Danysnz0 <dysanchez58@misena.edu.co>
2021-11-22 19:57:23 -0500

    Cambio en inicio

--- src/components/Inicio.js
+++ src/components/Inicio.js
@@ -1,40 +1,31 @@
 import React from 'react'
 import Slideshow from './Slideshow';
+import {Container, Col, Image, Row} from 'react-bootstrap';
 
 
 
 export default function Inicio() {
     return (
-        <main>
+        <div>
+            <div>
             <Slideshow/>
-        
+            </div>
+            
                    <Container>
-             <Row>
-               <Col xs={6} md={4}>
-                 <Image src="holder.js/171x180" rounded />
-               </Col>
-               <Col xs={6} md={4}>
-                 <Image src="holder.js/171x180" roundedCircle />
-               </Col>
-               <Col xs={6} md={4}>
-                 <Image src="holder.js/171x180" thumbnail />
-               </Col>
+                        <Row>
+                    <Col xs={6} md={4}>
+                    <Image src="holder.js/171x180" rounded />
+                    </Col>
+                    <Col xs={6} md={4}>
+                        <Image src="holder.js/171x180" roundedCircle />
+                    </Col>
+                        <Col xs={6} md={4}>
+                    <Image src="holder.js/171x180" thumbnail />
+                    </Col>
              </Row>
             </Container>
 
-            </main>     
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-
-            
+        </div> 
         
     )
 }
